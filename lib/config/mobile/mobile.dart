cce29ddaf522e191e73811f55f19c5042043aa73
mathru <user@mathru.net>
2022-06-08 11:29:04 +0900

    Linter対応

--- lib/config/mobile/mobile.dart
+++ lib/config/mobile/mobile.dart
@@ -7,7 +7,6 @@ import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
-import 'package:katana/katana.dart';
 import 'package:katana_flutter/katana_flutter.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
